2cb1dc4f32fef97e1a289a447370651ebc0ad383
tanmayrane18 <tanmayrane51@gmail.com>
2024-08-17 17:21:01 +0530

    meal item screen created

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -8,7 +8,9 @@ const CategoriesScreen = ({ navigation }) => {
   function renderCategoryItem(itemData) {
 
     function pressHandler() {
-        navigation.navigate("MealsOverview")
+        navigation.navigate("MealsOverview", {
+            categoryId: itemData.item.id
+        })
     }
 
     return (
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({ route}) => {
+    const categoryId = route.params.categoryId
+
+    const displayedMeals = MEALS.filter( mealItem => {
+        return mealItem.categoryIds.includes(categoryId)
+    })
+
+    const renderMealItem = (itemData) => {
+        return <MealItem  title={itemData.item.title} />
+    }
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen</Text>
+      <FlatList 
+        data={displayedMeals}
+        keyExtractor={ itemData => itemData.id}
+        renderItem={renderMealItem}
+      />
     </View>
   )
 }
